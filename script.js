db84f225211f8d31162cff2b2f9ecff3679de6ad
Ikechukwu Anyikwa <austineanyikwa@gmail.com>
2024-08-28 14:26:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('play-song').addEventListener('click', function () {
+    var iframe = document.getElementById('soundcloud-player');
+    iframe.style.display = 'none'; // Ensure the iframe is hidden
+    iframe.src += "&auto_play=true"; // Start the music
+});
+
+// Show the modal on page load
+$(document).ready(function() {
+    $('#audioPermissionModal').modal('show');
+});
+
+// Handle the audio permission button click
+document.getElementById('allow-audio').addEventListener('click', function() {
+    var iframe = document.getElementById('soundcloud-player');
+    // Play the audio
+    iframe.contentWindow.postMessage('{"method":"play"}', '*');
+    // Hide the modal
+    $('#audioPermissionModal').modal('hide');
+    // Show the main content
+    document.getElementById('main-content').style.display = 'flex';
+});
